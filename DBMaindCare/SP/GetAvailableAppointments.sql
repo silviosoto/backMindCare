7e071adfa85b81fd5abfa998e9dd3f185b335649
silviosoto <silviojsoto@gmail.com>
2025-03-16 11:55:03 -0500

    cambios en base de datos

---
+++ DBMaindCare/SP/GetAvailableAppointments.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[GetAvailableAppointments]
+    @IdMedico INT,
+    @Fecha DATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @date datetime = @Fecha
+   
+    SELECT HoraInicio, @date as hoy, cita.Hora
+    FROM Agenda 
+    left join cita 
+        on cita.Idpsicologo = Agenda.Idpsicologo
+        and cita.Hora = agenda.HoraInicio
+        and cita.fecha =  @Fecha
+    where 
+        anio = YEAR(@date) and cita.Hora is null 
+    order by
+	    DiaSemana, Agenda.HoraInicio
+END;
---
+++ DBMaindCare/Tables/cita.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[cita]
+(
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    Idpsicologo INT NOT NULL,
+    Idpaciente INT NOT NULL,
+    fecha datetime NOT NULL,
+    Hora TIME NOT NULL,
+    [FechaCreacion] DATETIME NULL, 
+    [FechaActualizacion] DATETIME NULL, 
+  
+    CONSTRAINT [FK_cita_psicologo] FOREIGN KEY ([Idpsicologo]) REFERENCES psicologo(id)
+
+)
